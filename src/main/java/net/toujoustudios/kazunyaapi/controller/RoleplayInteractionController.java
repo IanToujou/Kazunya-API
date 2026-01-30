096d7bcc9bf530bc2f5668679cfc8462ae002ed9
ibour <ianbour2016@gmail.com>
2026-01-30 15:47:36 +0100

    :label: Changed IDs to int

--- src/main/java/net/toujoustudios/kazunyaapi/controller/RoleplayInteractionController.java
+++ src/main/java/net/toujoustudios/kazunyaapi/controller/RoleplayInteractionController.java
@@ -3,9 +3,10 @@ package net.toujoustudios.kazunyaapi.controller;
 import lombok.RequiredArgsConstructor;
 import net.toujoustudios.kazunyaapi.model.RoleplayInteraction;
 import net.toujoustudios.kazunyaapi.repository.RoleplayInteractionRepository;
+import net.toujoustudios.kazunyaapi.request.RoleplayInteractionRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +25,20 @@ public class RoleplayInteractionController {
     @GetMapping
     public Optional<RoleplayInteraction> get(int id) {
         return repository.findById(id);
+    }
+
+    @PostMapping
+    public void add(RoleplayInteractionRequest request) {
+        save(request, new RoleplayInteraction());
+    }
+
+    private RoleplayInteraction save(RoleplayInteractionRequest request, RoleplayInteraction o) {
+
+
+
+        o.setName(request.name());
+        return repository.save(o);
+
     }
 
 }
---
+++ src/main/java/net/toujoustudios/kazunyaapi/request/RoleplayInteractionRequest.java
@@ -0,0 +1,7 @@
+package net.toujoustudios.kazunyaapi.request;
+
+import java.util.List;
+
+public record RoleplayInteractionRequest(String name, List<Integer> images) {
+
+}
