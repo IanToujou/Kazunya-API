4edf73d3af7c3314a6ba60f0223e671c16b2271b
Ian Toujou <ian@toujoustudios.net>
2026-03-06 01:52:47 +0100

    Added new method to find by name

--- src/main/java/net/toujoustudios/kazunyaapi/repository/RoleplayInteractionRepository.java
+++ src/main/java/net/toujoustudios/kazunyaapi/repository/RoleplayInteractionRepository.java
@@ -3,5 +3,10 @@ package net.toujoustudios.kazunyaapi.repository;
 import net.toujoustudios.kazunyaapi.model.RoleplayInteraction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoleplayInteractionRepository extends JpaRepository<RoleplayInteraction, Integer> {
+
+    List<RoleplayInteraction> findByName(String name);
+
 }
